797a8a5d614e21fd593fce77a14c5f7c6d731f25
Chris Base <baseappsdev@gmail.com>
2022-02-13 19:26:25 -0600

    started color info with html notation

--- src/ColorPickerContainer.js
+++ src/ColorPickerContainer.js
@@ -13,7 +13,38 @@ const ColorPickerContainer = () => {
         <div id='wheelAndInfoContainer'>
           <div id='colorWheel' />
 
-          <div id='colorInfoContainer'></div>
+          <div id='colorInfoContainer'>
+            <div id='redContainer' className='colorSliderContainer'>
+              <div id='redSlider' className='colorSlider'></div>
+              <div id='redNumber' className='colorNumber'>
+                <p className='colorNumberText'>0</p>
+              </div>
+            </div>
+
+            <div id='greenContainer' className='colorSliderContainer'>
+              <div id='greenSlider' className='colorSlider'></div>
+              <div id='greenNumber' className='colorNumber'>
+                <p className='colorNumberText'>0</p>
+              </div>
+            </div>
+
+            <div id='blueContainer' className='colorSliderContainer'>
+              <div id='blueSlider' className='colorSlider'></div>
+              <div id='blueNumber' className='colorNumber'>
+                <p className='colorNumberText'>255</p>
+              </div>
+            </div>
+
+            <div id='htmlContainer' className='colorSliderContainer'>
+              <p id='htmlText'>HTML:</p>
+              <div id='htmlInputContainer'>
+                <p id='htmlInput'>#0000ff</p>
+              </div>
+              <div id='copyContainer'>
+                <div id='copyButton' />
+              </div>
+            </div>
+          </div>
         </div>
 
         <div id='prevColorContainer'></div>
